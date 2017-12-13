66cdfc9d0af517fe5f3b7874c6fb95d3faced662
Michael Brohl <mbrohl@apache.org>
2017-12-13 13:09:46 +0000

    Improved: Fixing defects reported by FindBugs, package org.apache.ofbiz.content.test. (OFBIZ-9817)
    Thanks Julian Leichert for reporting and providing the patch.
    
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-plugins/trunk@1818009 13f79535-47bb-0310-9956-ffa450edef68

--- lucene/src/main/java/org/apache/ofbiz/content/test/LuceneTests.java
+++ lucene/src/main/java/org/apache/ofbiz/content/test/LuceneTests.java
@@ -41,10 +41,12 @@ import org.apache.ofbiz.entity.GenericValue;
 import org.apache.ofbiz.entity.util.EntityQuery;
 import org.apache.ofbiz.service.ServiceUtil;
 import org.apache.ofbiz.service.testtools.OFBizTestCase;
+import org.apache.ofbiz.base.util.Debug;
 
 public class LuceneTests extends OFBizTestCase {
 
     protected GenericValue userLogin = null;
+    public final static String module = LuceneTests.class.getName();
 
     public LuceneTests(String name) {
         super(name);
@@ -68,7 +70,9 @@ public class LuceneTests extends OFBizTestCase {
         
         try {
             Thread.sleep(3000); // sleep 3 seconds to give enough time to the indexer to process the entries
-        } catch(Exception e) {}
+        } catch (InterruptedException e) {
+            Debug.logError("Thread interrupted :" + e, module);
+        }
         
         Directory directory = FSDirectory.open(new File(SearchWorker.getIndexPath("content")).toPath());
         
