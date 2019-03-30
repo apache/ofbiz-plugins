9ce999b656f59c64b8648f327aa96b16d6e62390
Rishi Solanki <rishi@apache.org>
2019-03-30 12:43:46 +0000

    [Implemented] Short Messaging Service(SMS) Gateway Integration. Added msg91 component to plugins. It uses msg91 services to send sms. An example to demonstrate how to use SMS gateway integration with OFBiz. (OFBIZ-10457) Thanks to Pritam Kute for your contribution and Michael Brohi for review and feedback.
    git-svn-id: https://svn.apache.org/repos/asf/ofbiz/ofbiz-plugins/trunk@1856618 13f79535-47bb-0310-9956-ffa450edef68

---
+++ msg91/src/main/java/org/apache/ofbiz/msg91/Msg91Services.java
@@ -0,0 +1,54 @@
+package org.apache.ofbiz.msg91;
+
+import org.apache.ofbiz.base.util.Debug;
+import org.apache.ofbiz.base.util.HttpClient;
+import org.apache.ofbiz.base.util.HttpClientException;
+import org.apache.ofbiz.base.util.UtilGenerics;
+import org.apache.ofbiz.entity.Delegator;
+import org.apache.ofbiz.entity.GenericEntityException;
+import org.apache.ofbiz.entity.GenericValue;
+import org.apache.ofbiz.entity.util.EntityQuery;
+import org.apache.ofbiz.service.DispatchContext;
+import org.apache.ofbiz.service.LocalDispatcher;
+import org.apache.ofbiz.service.ServiceUtil;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Msg91Services {
+    public final static String module = Msg91Services.class.getName();
+
+    public static Map<String, Object> sendMsg91Sms(DispatchContext ctx, Map<String, ? extends Object> context) {
+        Delegator delegator = ctx.getDelegator();
+        Map<String, Object> result = ServiceUtil.returnSuccess();
+        
+        List<String> numbers = UtilGenerics.checkList(context.get("numbers"));
+        String message = (String) context.get("message");
+
+        try {
+            GenericValue msg91GatewayConfig = EntityQuery.use(delegator).from("Msg91GatewayConfig").queryFirst();
+            if (msg91GatewayConfig != null) {
+                Map<String, Object> paramMap = new HashMap<String, Object>();
+                paramMap.put("country", msg91GatewayConfig.getString("country"));
+                paramMap.put("sender", msg91GatewayConfig.getString("sender"));
+                paramMap.put("route", msg91GatewayConfig.getString("route"));
+                paramMap.put("mobiles", numbers);
+                paramMap.put("message", message);
+                paramMap.put("authkey", msg91GatewayConfig.getString("authkey"));
+
+                HttpClient httpClient = new HttpClient(msg91GatewayConfig.getString("apiUrl"), paramMap);
+                httpClient.setHeader("content-type", "application/text");
+                String response = httpClient.get();
+                result.put("response", response);
+            } else {
+                Debug.logError("Message not sent as the telecom gateway configuration settings are not found", module);
+                return ServiceUtil.returnError("Message not sent as the telecom gateway configuration settings are not found");
+            }
+        } catch (GenericEntityException | HttpClientException e) {
+            Debug.logError(e.getMessage(), module);
+            return ServiceUtil.returnError(e.getMessage());
+        }
+        return result;
+    }
+}
