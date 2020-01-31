ea214e4842220d7958c903b01be3357e21d02b7d
Pierre Smits <pierresmits@apache.org>
2020-01-31 09:24:31 +0100

    Implemented: Have a CountryDimension load service
    (OFBIZ-10956)
    
    added: a groovy script to query tables and create new CountryDimension records
    added: the service to invoke the groovy script to load CountryDimension records
    modified: the quickInitDataWarehouse method to include the loadCountryDimension service

---
+++ bi/groovyScripts/DwhDimensionServices.groovy
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.ofbiz.base.util.Debug;
+
+// Dimension table(s) related functions
+
+def loadCountryDimension(){
+    queryListIterator = from("Geo").where("geoTypeId","COUNTRY")queryIterator();
+    while(country = queryListIterator.next()){
+        countryId = country.geoId;
+        dimRecord = getDimensionRecord("CountryDimension","countryId", countryId);
+        if(!dimRecord){
+            dimensionId = countryId;
+            newEntity = makeValue("CountryDimension");
+            newEntity.dimensionId = dimensionId;
+            newEntity.countryId = countryId;
+            newEntity.countryCode = country.geoCode;
+            newEntity.countryNumCode = country.geoSecCode;
+            countryTeleRecord = from("CountryTeleCode").where("countryCode", country.geoCode).queryOne();
+            if(countryTeleRecord){
+                newEntity.countryTeleCode = countryTeleRecord.teleCode;
+            };
+            newEntity.countryName = country.geoName;
+            newEntity.create();
+        };
+    };
+    queryListIterator.close();
+};
