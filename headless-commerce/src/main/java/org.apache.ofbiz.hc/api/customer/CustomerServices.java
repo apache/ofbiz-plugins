8a80cfc0e6ba7e51ad91041c5bd5b93cb0005d1f
Ravi Lodhi <lodhiravi1@gmail.com>
2020-10-22 12:04:22 +0530

    Implemented: Added the basic directory structure of the headless commerce plugin.

---
+++ headless-commerce/src/main/java/org.apache.ofbiz.hc/api/customer/CustomerServices.java
@@ -0,0 +1,7 @@
+package org.apache.ofbiz.hc.api.customer;
+
+public class CustomerServices {
+
+    public static final String MODULE = CustomerServices.class.getName();
+    public static final String RESOURCE = "PartyUiLabels";
+}
