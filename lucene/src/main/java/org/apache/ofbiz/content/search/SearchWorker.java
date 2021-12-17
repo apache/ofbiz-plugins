f000c69a0d11fb42f4817dc744a01e6a638551d3
Jacques Le Roux <jacques.le.roux@les7arts.com>
2021-12-17 05:14:19 +0100

    Fixed: Update Solr and Lucene to address several CVEs (including Log4j) (OFBIZ-12464)
    Solr is not yet available on Maven :/
    
    We will also need to update Tika, and I guess the list will continue...
    
    Conflicts handled by hand in lucene/build.gradle

--- lucene/src/main/java/org/apache/ofbiz/content/search/SearchWorker.java
+++ lucene/src/main/java/org/apache/ofbiz/content/search/SearchWorker.java
@@ -41,7 +41,7 @@ public final class SearchWorker {
 
     public static final String module = SearchWorker.class.getName();
 
-    private static final Version LUCENE_VERSION = Version.LUCENE_8_11_1;
+    private static final Version LUCENE_VERSION = Version.LUCENE_8_11_0;
 
     private SearchWorker() {}
 
