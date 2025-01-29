89da5a59205f33d28162ebbed6b77b6a47161f8b
cgaetan <gaetan.chaboussie@nereide.fr>
2025-01-29 16:22:25 +0100

    OFBIZ-12989: Pricat plugin migration

--- pricat/src/main/java/org/apache/ofbiz/pricat/PricatEvents.java
+++ pricat/src/main/java/org/apache/ofbiz/pricat/PricatEvents.java
@@ -28,7 +28,7 @@ import java.nio.file.Paths;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload2.jakarta.JakartaServletFileUpload;
 import org.apache.ofbiz.base.location.ComponentLocationResolver;
 import org.apache.ofbiz.base.util.Debug;
 import org.apache.ofbiz.base.util.FileUtil;
@@ -77,7 +77,7 @@ public class PricatEvents {
      * Upload a pricat.
      */
     public static String pricatUpload(HttpServletRequest request, HttpServletResponse response) {
-        boolean isMultiPart = ServletFileUpload.isMultipartContent(request);
+        boolean isMultiPart = JakartaServletFileUpload.isMultipartContent(request);
         if (isMultiPart) {
             return "parse_pricat";
         } else {
