08d773966aa53bbbff77284ab0f7b8a22fb8f198
Ashish Vijaywargiya <ashish@apache.org>
2026-08-21 13:01:24 +0530

    Restructure example component's test-run service and Jupiter test file locations
    - Move ExampleTestRunServices.groovy from src/main/groovy to src/test/groovy,
      since it only exists to trigger/poll test runs
    - Move the Test Run Services block in services.xml to the end of the file
      and update its location attributes to match
    - Move ExampleJupiterTests.groovy up one directory (out of the jupiter/
      subfolder) since the class name already conveys its purpose; update its
      package declaration and the jupiter-test-suite class-name in tests.xml
      to match

--- example/src/test/groovy/org/apache/ofbiz/example/test/ExampleJupiterTests.groovy
+++ example/src/test/groovy/org/apache/ofbiz/example/test/ExampleJupiterTests.groovy
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.ofbiz.example.test.jupiter
+package org.apache.ofbiz.example.test
 
 import org.apache.ofbiz.entity.GenericValue
 import org.apache.ofbiz.service.ServiceUtil
